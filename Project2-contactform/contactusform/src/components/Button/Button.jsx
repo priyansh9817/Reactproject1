d1b4f0b48e48c2cc85d7c45984f3c6925f561bd3
priyansh9817 <priyanshu9817.s@gmail.com>
2024-12-11 20:35:29 +0530

    Project2

--- Project2-contactform/contactusform/src/components/Button/Button.jsx
+++ Project2-contactform/contactusform/src/components/Button/Button.jsx
@@ -1,14 +1,13 @@
-import styles from "./Button.module.css";  
+import styles from "./Button.module.css";
 
-const Button = (props) => {
-  return (<>
-    <button className={props.isOutline ? styles.Outline_btn : styles.primary_btn}>{props.icons} {props.text}</button>
-    
-
-
-    </>
-
-
+const Button = ({ isOutline, icons, text, ...rest }) => {
+  return (
+    <button 
+      {...rest}
+      className={isOutline ? styles.Outline_btn : styles.primary_btn}
+    >
+      {icons} {text}
+    </button>
   );
 };
 
--- Project2-contactform/contactusform/src/components/ContactForm/Form.jsx
+++ Project2-contactform/contactusform/src/components/ContactForm/Form.jsx
@@ -1,22 +1,69 @@
 import styles from "./Form.module.css";
 import Button from "../Button/Button";
-import { MdMessage } from "react-icons/md";
-import { MdAddCall } from "react-icons/md";
+import { MdMessage, MdAddCall } from "react-icons/md";
 import { IoMail } from "react-icons/io5";
+import React,{useState} from "react";
+
 const Form = () => {
-    return <div className={styles.container}>
-        <div className={styles.contact_form}>
-            <div className={styles.topbutton}>
-                <div><Button text="VIA CHART SUPPORT" icons={<MdMessage fontSize="24px" />}></Button></div>
-                <div><Button text="VIA CAll" icons={<MdAddCall fontSize="24px" />}></Button></div>
+    const [name , setName] = useState("");
+    const [email , setemail] = useState("")
+    const [text , settext] = useState("")
+  
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        setName(event.target[0].value)
+        setemail(event.target[1].value)
+        settext(event.target[2].value)
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.contact_form}>
+                <div className={styles.topbutton}>
+                    <Button 
+                        text="VIA CHAT SUPPORT" 
+                        icons={<MdMessage fontSize="24px" />} 
+                    />
+                    <Button 
+                        text="VIA CALL" 
+                        icons={<MdAddCall fontSize="24px" />} 
+                    />
+                </div>
+                <Button 
+                    isOutline={true} 
+                    text="VIA EMAIL FORM" 
+                    icons={<IoMail fontSize="24px" />} 
+                />
+                 <form onSubmit={onSubmit} >
+                <div className={styles.form_container}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" />
+                </div>
+                <div className={styles.form_container}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" />
+                </div>
+                <div className={styles.form_container}>
+                    <label htmlFor="textarea">Textarea</label>
+                    <textarea name="" id="" rows='8'></textarea>
+                </div>
+                <div style={
+                    {
+                        display:"flex",
+                        justifyContent:"end",
+                    }
+                }>
+                <Button text="submit"/>
+                </div>
+                <div>{name + " " + email + " " + text}</div>
+            </form >
             </div>
-            <div><Button isOutline={true} 
-            text="VIA EMAIL FORM" icons={<IoMail fontSize="24px" />}></Button></div>
+           
+            <div className={styles.contact_image}>
+                <img src="/images/contact.svg" alt="Contact illustration" />
+            </div>
         </div>
-        <div className={styles.contact_image}>
-            <img src="/images/contact.svg" alt="" />
-        </div>
-    </div>
-}
+    );
+};
 
 export default Form;
